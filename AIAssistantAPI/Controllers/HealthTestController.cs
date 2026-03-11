e01e97bc94515f4de7bec3075f28bbf664401bb3
RahulKR-SMG <rahul.r@stanford-marine.com>
2026-03-11 14:36:57 +0400

    Healthtest included

---
+++ AIAssistantAPI/Controllers/HealthTestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIAssistantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            var dtfrom = DateTime.Now;
+           
+            // This endpoint is used to check the health of the API
+            return Ok(new { status = "Welcome to AI API V.0.0.1" });
+        }
+    }
+}
